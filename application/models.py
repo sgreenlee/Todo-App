043a1bac4469eef5f2ced30ba72b11fcf42cd6c8
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-10-10 19:42:30 -0400

    Implemented User model.

---
+++ application/models.py
@@ -0,0 +1,27 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+MAX_LENGTH = {
+    'user_email': 40,
+    'user_first_name': 20,
+    'user_last_name': 20
+}
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(
+        db.string(MAX_LENGTH['user_email']),
+        unique=True,
+        nullable=False
+        )
+    first_name = db.Column(
+        db.String(MAX_LENGTH['user_first_name']),
+        nullable=False
+        )
+    last_name = db.Column(
+        db.String(MAX_LENGTH['user_last_name']),
+        nullable=False
+        )
