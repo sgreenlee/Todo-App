c0038b1c15e8f29d1fe909d410ef1238f909977b
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-10-16 16:41:54 -0400

    Implemented LoginForm class.

---
+++ application/auth/forms.py
@@ -0,0 +1,13 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Email
+from ..models import MAX_LENGTH
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(),
+                                             Length(1, MAX_LENGTH['email']),
+                                             Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
