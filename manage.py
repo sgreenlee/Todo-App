320bf0cc0bd94bb69fcadb357315168a3f1a3b31
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-10-19 15:15:00 -0400

    Enable database migrations with Flask-migrate

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 from flask.ext.script import Shell, Manager, Server
+from flask.ext.migrate import Migrate, MigrateCommand
 from application import create_app, db
 from application.models import User
 
 app = create_app('development')
 manager = Manager(app)
+migrate = Migrate(app, db)
+
 
 def make_shell_context():
     return dict(app=app, db=db, User=User)
@@ -12,6 +15,10 @@ manager.add_command('shell', Shell(make_context=make_shell_context))
 # development server configuration
 server = Server(host='0.0.0.0', port=8000)
 manager.add_command('runserver', server)
+
+# database migrations
+manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def test():
