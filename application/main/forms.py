e7291e154c7d2662b72fd0afcc7a26fb0ef569dd
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-10-26 16:26:52 -0400

    Implemented NewTaskForm

--- application/main/forms.py
+++ application/main/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, ValidationError
+from wtforms import StringField, DateField, SubmitField, ValidationError
 from wtforms.validators import Required, Email, Length
-from ..models import MAX_LENGTH, User
+from ..models import MAX_LENGTH, User, Task
 
 
 class EditProfileForm(Form):
@@ -15,3 +15,12 @@ class EditProfileForm(Form):
     email.validators = [Required(), Length(1, MAX_LENGTH['email']), Email()]
     first_name.validators = [Length(1, MAX_LENGTH['first_name'])]
     last_name.validators = [Length(1, MAX_LENGTH['last_name'])]
+
+
+class NewTaskForm(Form):
+    """Form for creating a new task."""
+    description = StringField('Description')
+    description.validators = [Length(1, MAX_LENGTH['task_desc'])]
+    deadline = DateField('Deadline')
+    priority = StringField('Priority')
+    priority.validators = [Length(1, MAX_LENGTH['task_priority'])]
