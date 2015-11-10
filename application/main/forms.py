5a4c99c74dde7efacf9fbf55470ce306e8a25f9e
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-11-10 18:50:00 -0500

    Fixed form validators.

--- application/main/forms.py
+++ application/main/forms.py
@@ -8,21 +8,24 @@ from ..models import MAX_LENGTH, User, Task
 class EditProfileForm(Form):
     """Profile Edit Form."""
     # fields
-    email = StringField('Email')
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
+    email = StringField(
+        'Email',
+        validators=[Required(), Length(1, MAX_LENGTH['email']), Email()])
+    first_name = StringField(
+        'First Name',
+        validators=[Length(0, MAX_LENGTH['first_name'])])
+    last_name = StringField(
+        'Last Name',
+        validators=[Length(0, MAX_LENGTH['last_name'])])
     submit = SubmitField('Update Your Profile')
-    # validators
-    email.validators = [Required(), Length(1, MAX_LENGTH['email']), Email()]
-    first_name.validators = [Length(1, MAX_LENGTH['first_name'])]
-    last_name.validators = [Length(1, MAX_LENGTH['last_name'])]
 
 
 class NewTaskForm(Form):
     """Form for creating a new task."""
-    description = StringField('Description')
-    description.validators = [Required(), Length(1, MAX_LENGTH['task_desc'])]
+    description = StringField(
+        'Description',
+        validators=[Required(), Length(1, MAX_LENGTH['task_desc'])])
     deadline = DateField('Deadline', validators=[Optional()])
-    priority = StringField('Priority')
-    priority.validators = [Length(1, MAX_LENGTH['task_priority'])]
+    priority = StringField(
+        'Priority', validators=[Length(0, MAX_LENGTH['task_priority'])])
     submit = SubmitField('Create Task')
